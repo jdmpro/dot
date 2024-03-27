9acd472a742fad2934b7686ee1fb574cd233f763
jdmpro <58115966+jdmpro@users.noreply.github.com>
2024-03-26 21:00:38 -0700

    Update dotfiles.sh

--- install-scripts/dotfiles.sh
+++ install-scripts/dotfiles.sh
@@ -19,7 +19,7 @@ if [ -f Hyprland-Dots.tar.gz ]; then
   existing_version=$(echo Hyprland-Dots.tar.gz | grep -oP 'v\d+\.\d+\.\d+' | sed 's/v//')
 
   # Fetch the tag_name for the latest release using the GitHub API
-  latest_version=$(curl -s https://api.github.com/repos/JaKooLit/Hyprland-Dots/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
+  latest_version=$(curl -s https://api.github.com/repos/jdmpro/dotfiles/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
 
   # Check if versions match
   if [ "$existing_version" = "$latest_version" ]; then
@@ -38,7 +38,7 @@ if [ -f Hyprland-Dots.tar.gz ]; then
 		echo -e "${NOTE} Proceeding to download the latest release." 2>&1 | tee -a "../Install-Logs/install-$(date +'%d-%H%M%S')_dotfiles.log"
 		
 		# Delete existing directories starting with JaKooLit-Hyprland-Dots
-      find . -type d -name 'JaKooLit-Hyprland-Dots*' -exec rm -rf {} +
+      find . -type d -name 'Shredder-Hyprland-Dots*' -exec rm -rf {} +
       rm -f Hyprland-Dots.tar.gz
       printf "${WARN} Removed existing Hyprland-Dots.tar.gz.\n"
     else
@@ -51,7 +51,7 @@ fi
 printf "${NOTE} Downloading the latest Hyprland source code release...\n"
 
 # Fetch the tag name for the latest release using the GitHub API
-latest_tag=$(curl -s https://api.github.com/repos/JaKooLit/Hyprland-Dots/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+latest_tag=$(curl -s https://api.github.com/repos/jdmpro/dotfiles/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 
 # Check if the tag is obtained successfully
 if [ -z "$latest_tag" ]; then
@@ -60,7 +60,7 @@ if [ -z "$latest_tag" ]; then
 fi
 
 # Fetch the tarball URL for the latest release using the GitHub API
-latest_tarball_url=$(curl -s https://api.github.com/repos/JaKooLit/Hyprland-Dots/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
+latest_tarball_url=$(curl -s https://api.github.com/repos/jdmpro/dotfiles/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
 
 # Check if the URL is obtained successfully
 if [ -z "$latest_tarball_url" ]; then
@@ -77,15 +77,15 @@ if curl -L "$latest_tarball_url" -o "$file_name"; then
   tar -xzf "$file_name" || exit 1
 
   # delete existing Hyprland-Dots
-  rm -rf JaKooLit-Hyprland-Dots
+  rm -rf Shredder-Hyprland-Dots
 
   # Identify the extracted directory
   extracted_directory=$(tar -tf "$file_name" | grep -o '^[^/]\+' | uniq)
 
   # Rename the extracted directory to JaKooLit-Hyprland-Dots
-  mv "$extracted_directory" JaKooLit-Hyprland-Dots || exit 1
+  mv "$extracted_directory" Shredder-Hyprland-Dots || exit 1
 
-  cd "JaKooLit-Hyprland-Dots" || exit 1
+  cd "Shredder-Hyprland-Dots" || exit 1
 
   # Set execute permission for copy.sh and execute it
   chmod +x copy.sh
